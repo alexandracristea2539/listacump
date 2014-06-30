14cb2916fa9bf1bc65b0cad21154b7956455f149
alexandracristea2539 <crisalexandra2005@yahoo.com>
2014-06-30 17:46:08 +0300

    lista cumparaturi

---
+++ b.js
@@ -0,0 +1,36 @@
+/**
+ * Created by alexandra.cristea on 6/30/2014.
+ */
+
+var app = angular.module('Store',[]);
+
+app.controller('ShopCtrl', ['$scope', function($scope) {
+
+    $scope.items = [
+        {id: 1, text: 'Lapte', bought: false},
+        {id: 2, text: 'Oua', bought: true},
+        {id: 3, text: 'Cola', bought: false}
+    ];
+
+    $scope.clearBought = function() {
+        $scope.items = filter($scope.items, function (item) {
+            return !item.bought;
+        });
+    }
+    $scope.addItem = function() {
+        $scope.items.push({text: $scope.itemEntry, bought: false, id: ($scope.items.length + 1) });
+        $scope.itemEntry = '';
+    }
+
+   $scope.addItem1 = function() {
+
+        $scope.items.push({text: $scope.itemEntry, bought: false, id: ($scope.items.length + 1) });
+        $scope.itemEntry = '';
+    }
+
+    $scope.isBought = function(bought) {
+        return (bought) ? 'bought' : 'not-bought';
+
+    }
+
+}]);
