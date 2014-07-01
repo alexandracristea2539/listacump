cc1b923dadf00fa032db393f9d18695dac61055f
alexandracristea2539 <crisalexandra2005@yahoo.com>
2014-07-01 16:05:05 +0300

    toate functionalitatile

--- b.js
+++ b.js
@@ -3,34 +3,66 @@
  */
 
 var app = angular.module('Store',[]);
-//fhdsfh
-app.controller('ShopCtrl', ['$scope', function($scope) {
+
+
+app.controller('ShopCtrl', ['$scope', function ($scope) {
 
     $scope.items = [
         {id: 1, text: 'Lapte', bought: false},
-        {id: 2, text: 'Oua', bought: true},
+        {id: 2, text: 'Oua',bought: false},
         {id: 3, text: 'Cola', bought: false}
     ];
 
-    $scope.clearBought = function() {
-        $scope.items = filter($scope.items, function (item) {
-            return !item.bought;
-        });
-    }
+    $scope.control = function () {
+        $scope.checked = !$scope.checked;
+    };
+
+    $scope.produs = {
+        text: '',
+        descriere: ''
+    };
     $scope.addItem = function() {
-        $scope.items.push({text: $scope.itemEntry, bought: false, id: ($scope.items.length + 1) });
-        $scope.itemEntry = '';
-    }
+        $scope.items.push($scope.produs);
+        $scope.produs={
+            text: '',
+            descriere: ''};
+    };
 
-   $scope.addItem1 = function() {
+    $scope.clearBought = function() {
+         var oldItems=$scope.items;
+         $scope.items=[];
+        angular.forEach(oldItems,function(item) {
+            if (!item.bought) $scope.items.push(item)
+        });
 
-        $scope.items.push({text: $scope.itemEntry, bought: false, id: ($scope.items.length + 1) });
-        $scope.itemEntry = '';
-    }
+
+
+    };
+
 
     $scope.isBought = function(bought) {
         return (bought) ? 'bought' : 'not-bought';
 
     }
+
+}]);
+
+app.controller('EditCtrl', ['$scope', function($scope) {
+
+    $scope.editorEnabled = false;
+    $scope.enableEditor = function() {
+        $scope.editorEnabled = true;
+        $scope.editableTitle = $scope.item.text;
+        $scope.editableTitle1 = $scope.item.descriere;
+    };
+    $scope.disableEditor = function() {
+        $scope.editorEnabled = false;
+    };
+    $scope.save = function() {
+        $scope.item.text = $scope.editableTitle;
+        $scope.item.descriere = $scope.editableTitle1;
+        $scope.disableEditor();
+    };
+
 
 }]);
